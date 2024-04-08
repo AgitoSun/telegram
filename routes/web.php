a93864e733aa2c7382b19672c5845da4561818b8
AgitoSun <zeke1@mail.ru>
2024-04-08 19:40:47 +0300

    init laravel

--- routes/web.php
+++ routes/web.php
@@ -39,8 +39,8 @@ use Illuminate\Support\Facades\Http;
 //    'number' => 1
 //]);
 //\App\Telegram\Helpers\InlineButton::link('Вторая кнопка', 'https://prog-time.ru/course/bot-v-telegram-5/');
-\App\Telegram\Helpers\KeyboardButton::add('Тестовая кнопка');
-dd(\App\Facades\Telegram::inlineButtons(5330525821, 'Клавиатура', \App\Telegram\Helpers\KeyboardButton::$buttons)->send());
+//\App\Telegram\Helpers\KeyboardButton::add('Тестовая кнопка');
+//dd(\App\Facades\Telegram::inlineButtons(5330525821, 'Клавиатура', \App\Telegram\Helpers\KeyboardButton::$buttons)->send());
 
 
 Route::get('/', function () {
