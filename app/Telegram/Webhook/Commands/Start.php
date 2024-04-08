0c1c64465009c00171acbefcc12099f91913ecce
AgitoSun <zeke1@mail.ru>
2024-04-08 16:43:45 +0300

    init laravel

--- app/Telegram/Webhook/Commands/Start.php
+++ app/Telegram/Webhook/Commands/Start.php
@@ -2,12 +2,14 @@
 
 namespace App\Telegram\Webhook\Commands;
 
+use App\Facades\Telegram;
 use App\Telegram\Webhook\Webhook;
 
 class Start extends Webhook
 {
     public function run()
     {
-       return $this->telegram->message(5330525821, 'Привет, спасибо что подписался')->send();
+//       return $this->telegram->message(5330525821, 'Привет, спасибо что подписался')->send();
+       Telegram::message(5330525821, 'Привет, спасибо что подписался')->send();
     }
 }
