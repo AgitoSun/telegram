0c08805244ed3a926655d48ba8484a14ea001b76
AgitoSun <zeke1@mail.ru>
2024-04-10 17:36:18 +0300

    commit

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -12,7 +12,7 @@ class StartCommand extends Command
 
     public function handle()
     {
-        $chat_id = json_decode(Telegram::getWebhookUpdate());
+        $chat_id = json_decode(Telegram::getUpdates()['message']);
         $this->replyWithMessage([
             'text' => 'Hey, there! Welcome to our bot!'.$chat_id,
             'chat_id' => 5330525821
