a2ed6884a3c839e82d0475c5d785f2ce103d9f13
AgitoSun <zeke1@mail.ru>
2024-04-10 16:51:43 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/webhook-data', function () {
     dd(\Illuminate\Support\Facades\Cache::get('webhook-data'));
 });
 
-dd(Telegram::getMe()->get('id'));
+dd(Telegram::bot());
 
 //$response = Telegram::setWebhook(['url' => 'https://podlegaevm.ru/api/webhook']);
 //dd($response);
