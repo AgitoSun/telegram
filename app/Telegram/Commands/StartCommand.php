bf4c79dbd4d1753b9bf78e81b4425ca6540f4cfc
AgitoSun <zeke1@mail.ru>
2024-04-10 17:52:55 +0300

    commit

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -8,19 +8,12 @@ use Telegram\Bot\Laravel\Facades\Telegram;
 
 class StartCommand extends Command
 {
-    protected Request $request;
-
     protected string $name = 'start';
     protected string $description = 'Start Command to get you started';
 
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-
     public function handle()
     {
-        $telegram_id =  $this->request->input('message')['from']['id'];
+        $telegram_id =  Telegram::getUpdates()['message']['from']['id'];
 
         $this->replyWithMessage([
             'text' => 'Hey, there! Welcome to our bot!',
