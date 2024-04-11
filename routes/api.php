3be9d0f9777407e37cf20b914532b0cc7d805881
AgitoSun <zeke1@mail.ru>
2024-04-11 10:36:42 +0300

    commit

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
@@ -24,6 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/webhook', function () {
     $update = Telegram::commandsHandler(true);
     $updates = Telegram::getWebhookUpdate();
+    Cache::forever('webhook-data', '123123123');
     // Commands handler method returns the Update object.
     // So you can further process $update object
     // to however you want.
