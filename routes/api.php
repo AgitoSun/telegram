339e50e1328307b8a951c709e21390855920a721
AgitoSun <zeke1@mail.ru>
2024-04-11 10:13:17 +0300

    commit

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('webhook', [\App\Http\Controllers\BotController::class, 'index']);
+Route::post('/webhook', [\App\Http\Controllers\BotController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
