94e11faec927d14b1831203559cdc1ad34dc9aca
AgitoSun <zeke1@mail.ru>
2024-04-11 17:37:30 +0300

    commitwe

--- app/Telegraph/Webhook.php
+++ app/Telegraph/Webhook.php
@@ -18,7 +18,7 @@ class Webhook extends WebhookHandler
         $this->chat
             ->message('Поиграем?')
             ->keyboard(Keyboard::make()->buttons([
-                Button::make('-1-')->action('game')->param('value', 1),
+                Button::make('-1-')->action(Game::class)->param('value', 1),
                 Button::make('-2-')->action('game')->param('value', 2),
                 Button::make('-3-')->action('game')->param('value', 3),
             ]))->send();
