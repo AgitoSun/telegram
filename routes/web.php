7f7310f7cf8b604e179ed4d27ef91d0294ddd385
AgitoSun <zeke1@mail.ru>
2024-04-11 09:26:20 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Route::get('/webhook-data', function () {
 //    dd(\Illuminate\Support\Facades\Cache::get('webhook-data'));
-    dd(Telegram::getWebhookUpdate()->message);
+    dd(Telegram::getWebhookUpdate()['message']);
 });
 
 
