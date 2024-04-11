fcf7bef2f97703e559442226187fa0f0df354be7
AgitoSun <zeke1@mail.ru>
2024-04-11 16:02:09 +0300

    commitwe

--- app/Http/Telegraph/Webhook.php
+++ app/Http/Telegraph/Webhook.php
@@ -3,11 +3,23 @@
 namespace App\Http\Telegraph;
 
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use DefStudio\Telegraph\Keyboard\Button;
+use DefStudio\Telegraph\Keyboard\Keyboard;
 
 class Webhook extends WebhookHandler
 {
     public function start()
     {
         $this->chat->html('Привет')->send();
+
+        sleep(1);
+
+        $this->chat
+            ->message('Поиграем?')
+            ->keyboard(Keyboard::make()->buttons([
+                Button::make('-1-')->action('game')->param('value', 1),
+                Button::make('-2-')->action('game')->param('value', 2),
+                Button::make('-3-')->action('game')->param('value', 3),
+            ]))->send();
     }
 }
