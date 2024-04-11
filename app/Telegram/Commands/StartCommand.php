5d0177a4f45664142a1e1ba9a1ada62621a01d83
AgitoSun <zeke1@mail.ru>
2024-04-11 10:45:59 +0300

    commit

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -9,6 +9,7 @@ use Telegram\Bot\Laravel\Facades\Telegram;
 class StartCommand extends Command
 {
     protected string $name = 'start';
+    protected array $aliases = ['subscribe'];
     protected string $description = 'Start Command to get you started';
 
     public function handle()
